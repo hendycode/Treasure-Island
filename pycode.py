0c522bfb1863b379055877e1885d9ea73f9dad12
hendycode <ratemoneon@gmail.com>
2025-05-27 16:50:52 +0300

    practise

---
+++ pycode.py
@@ -0,0 +1,8 @@
+print("Welcome to treasure Island")
+direction = input("So your mission today is to find the treasure. Have a great time hunting. \nWould you like to go left or right? ")
+
+
+if direction == "left":
+    pass
+else:
+    print("You have fell into a hole. Game over.")
